3402265d146aab4a9bbb2b4bd23a5f3c059b9d84
Kolb22 <88410484+Kolb22@users.noreply.github.com>
2022-02-06 12:24:07 -0400

    categories inserts

---
+++ inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO `demo`.`category` (`id`, `name`) VALUES ('1', 'General');
+INSERT INTO `demo`.`category` (`id`, `name`) VALUES ('2', 'Technology');
+INSERT INTO `demo`.`category` (`id`, `name`) VALUES ('3', 'Random');
